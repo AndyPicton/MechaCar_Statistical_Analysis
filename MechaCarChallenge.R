5f10824c13d8dbf7ec1a203ed04097f97337f250
AndyPicton <andyspicton@yahoo.com>
2022-06-19 16:16:48 -0500

    first

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,1 +1,12 @@
+library(dplyr)
 
+# Deliverable 1
+
+#Import and read csv file as DF
+mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors=F)
+
+#Perform linear regression
+lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,mpg_df)
+
+# Summary for linear regression
+summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,mpg_df))
